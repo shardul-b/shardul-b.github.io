e3b7cda685df57a4829c456d16c05eb1c1d0c259
ShardulB <shardulbirje15@gmail.com>
2020-07-20 19:45:48 +0530

    Added Chat section

---
+++ instagram-chat/script.js
@@ -0,0 +1,18 @@
+//Handles Id's
+const ids=(val)=>{
+	let value=document.getElementById(val);
+	return value;
+};
+//Handles Classes
+const classes=(val)=>{
+	let values=document.getElementsByClassName(val);
+	return values;
+};
+//Element Creation
+const creator=(val)=>{
+	let created=document.createElement(val);
+	return created;
+};
+const sender_messages=[
+	'Hello',
+];
