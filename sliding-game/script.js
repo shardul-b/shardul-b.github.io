be318ce53ae98bc53b6b144740a2ee7a15986deb
ShardulB <58384725+shardul-b@users.noreply.github.com>
2023-03-17 00:30:02 +0530

    updated sliding image script

--- sliding-game/script.js
+++ sliding-game/script.js
@@ -6,8 +6,6 @@ const result = document.getElementById('result');
 let currentElement = '';
 let movesCount,
   imagesArr = [];
-let initialX = 0,
-  initialY = 0;
 const is_touch_device = () => {
   try {
     //We try to create TouchEvent (it would fail for desktops and throw error)
@@ -56,36 +54,27 @@ const gridGenerator = () => {
     for (let j = 0; j < 3; j++) {
       let div = document.createElement('div');
       div.setAttribute('data-position', `${i}_${j}`);
-      div.addEventListener('dragstart', dragStart);
-      div.addEventListener('dragover', dragOver); 
-      div.addEventListener('drop', drop); 
-      div.addEventListener('touchstart', dragStart, false);
-      div.addEventListener('touchmove', drop, false);
+      div.addEventListener('click', selectImage);
       div.classList.add('image-container');
-      div.innerHTML = `<img src="./images/image_part_00${imagesArr[count]}.png"  class="image" data-index="${imagesArr[count]}"/>`;
+      div.innerHTML = `<img src="./images/image_part_00${
+        imagesArr[count]
+      }.png"  class="image ${
+        imagesArr[count] == 9 ? 'target' : ''
+      }"  data-index="${imagesArr[count]}"/>`;
       count += 1;
       container.appendChild(div);
     }
   }
 };
 
-//start dragging
-const dragStart = (e) => {
-  initialX = is_touch_device() ? e.touches[0].clientX : e.clientX;
-  initialY = is_touch_device() ? e.touches[0].clientY : e.clientY;
+//click the image
+const selectImage = (e) => {
+  e.preventDefault();
+ 
   //set currentElement
   currentElement = e.target;
-};
-const dragOver = (e) => {
-  e.preventDefault();
-};
-//drop the image
-const drop = (e) => {
-  e.preventDefault();
-  let newX = is_touch_device() ? e.touches[0].clientX : e.clientX;
-  let newY = is_touch_device() ? e.touches[0].clientY : e.clientY;
-  //set targetElement (where we drop the picked element).. It is based on mouse position
-  let targetElement = document.elementFromPoint(newX, newY);
+  //target (blank image)
+  let targetElement = document.querySelector('.target');
   let currentParent = currentElement.parentElement;
   let targetParent = targetElement.parentElement;
 
@@ -93,42 +82,40 @@ const drop = (e) => {
   const [row1, col1] = getCoords(currentParent);
   const [row2, col2] = getCoords(targetParent);
 
-  //Check if the target is blank
-  if (targetElement.getAttribute('src').includes('009')) {
-    if (checkAdjacent(row1, row2, col1, col2)) {
-      //swap
-      currentElement.remove();
-      targetElement.remove();
-      //get image index(to be used later for manipulating array)
-      let currentIndex = parseInt(currentElement.getAttribute('data-index'));
-      let targetIndex = parseInt(targetElement.getAttribute('data-index'));
-      //swap index
-      currentElement.setAttribute('data-index', targetIndex);
-      targetElement.setAttribute('data-index', currentIndex);
-      //swap images
-      currentParent.appendChild(targetElement);
-      targetParent.appendChild(currentElement);
-      //array swaps
-      let currentArrIndex = imagesArr.indexOf(currentIndex);
-      let targetArrIndex = imagesArr.indexOf(targetIndex);
-      [imagesArr[currentArrIndex], imagesArr[targetArrIndex]] = [
-        imagesArr[targetArrIndex],
-        imagesArr[currentArrIndex],
-      ];
-      //win condition
-      if (imagesArr.join('') == '123456789') {
-        setTimeout(() => {
-          //When game ends display the cover screen again
-          coverScreen.classList.remove('hide');
-          container.classList.add('hide');
-          result.innerText = `Total Moves: ${movesCount}`;
-          startButton.innerText = 'Restart Game';
-        }, 1000);
-      }
-      //increment an display moves
-      movesCount += 1;
-      moves.innerText = `Moves: ${movesCount}`;
+
+  if (checkAdjacent(row1, row2, col1, col2)) {
+    //swap
+    currentElement.remove();
+    targetElement.remove();
+    //get image index(to be used later for manipulating array)
+    let currentIndex = parseInt(currentElement.getAttribute('data-index'));
+    let targetIndex = parseInt(targetElement.getAttribute('data-index'));
+    //swap index
+    currentElement.setAttribute('data-index', targetIndex);
+    targetElement.setAttribute('data-index', currentIndex);
+    //swap images
+    currentParent.appendChild(targetElement);
+    targetParent.appendChild(currentElement);
+    //array swaps
+    let currentArrIndex = imagesArr.indexOf(currentIndex);
+    let targetArrIndex = imagesArr.indexOf(targetIndex);
+    [imagesArr[currentArrIndex], imagesArr[targetArrIndex]] = [
+      imagesArr[targetArrIndex],
+      imagesArr[currentArrIndex],
+    ];
+    //win condition
+    if (imagesArr.join('') == '123456789') {
+      setTimeout(() => {
+        //When game ends display the cover screen again
+        coverScreen.classList.remove('hide');
+        container.classList.add('hide');
+        result.innerText = `Total Moves: ${movesCount}`;
+        startButton.innerText = 'Restart Game';
+      }, 1000);
     }
+    //increment an display moves
+    movesCount += 1;
+    moves.innerText = `Moves: ${movesCount}`;
   }
 };
 //Start button click should display the container
